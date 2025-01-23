32e594440f950ddcd0bcd99ca4d1529dc4147cb8
Onome <Onome@onomes-mbp.home>
2025-01-23 05:26:29 +0000

    Updated

--- payment_API/payment_API/settings.py
+++ payment_API/payment_API/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-c_8wb0fw409i4-r=j7*ggskm=v_(u%2y+#+3yie0lafeg_3$0o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
